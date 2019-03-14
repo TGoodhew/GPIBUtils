787a27893be143d4a653c8d1d121fc85bb8f874d
Tony Goodhew <tony_goodhew@hotmail.com>
2019-03-14 12:47:43 -0700

    Initial project files

---
+++ HP438A/HP438ACmdLib/HP$38ACmdLib.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HP438ACmdLib
+{
+    public class HP438ACmds
+    {
+    }
+}
