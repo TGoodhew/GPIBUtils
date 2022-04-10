e0792cf65abcafc798dc51ed6089d0ed9c3e37d0
Tony Goodhew <tony@schnauzergroup.com>
2022-04-10 11:52:05 -0700

    Initial 8673B device created

---
+++ HP8973B-Test/HP8673B-Test/HP8673B-Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HP8673B;
+
+namespace HP8673B_Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HP8673B.Device measuringReceiver = new Device(@"GPIB0::19::INSTR");
+        }
+    }
+}
