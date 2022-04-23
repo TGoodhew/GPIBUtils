e87955a51aa290ff2372aee5d88794c3b01de39f
Tony Goodhew <tony@schnauzergroup.com>
2022-04-23 16:26:07 -0700

    Added RF Power calibration factor loading, sensor zeroing and sensor calibration.

--- HP8902A-Test/HP8902A/Device.cs
+++ HP8902A-Test/HP8902A/Device.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 using Ivi.Visa.FormattedIO;
 using NationalInstruments.Visa;
 using Ivi.Visa;
+using System.IO;
+using System.Text.Json;
 
 namespace HP8902A
 {
@@ -29,6 +31,20 @@ namespace HP8902A
         UnOrRecalibrated = 0x20
     }
 
+    public class CalibrationFactor
+    {
+        public decimal Frequency { get; set; }
+        public decimal CalFactor { get; set; }
+
+        public CalibrationFactor() { }
+
+        public CalibrationFactor(double frequency, double calFactor)
+        {
+            Frequency = (decimal)frequency;
+            CalFactor = (decimal)calFactor;
+        }
+    }
+
     public class Device
     {
         public string gpibAddress { get; }
@@ -36,6 +52,8 @@ namespace HP8902A
         private GpibSession gpibSession;
         private ResourceManager resManager;
         private SemaphoreSlim srqWait = new SemaphoreSlim(0, 1); // use a semaphore to wait for the SRQ events
+
+        private List<CalibrationFactor> CalFactors;
 
         public Device (string GPIBAddress)
         {
@@ -233,6 +251,83 @@ namespace HP8902A
             return ReadSciValue();
         }
 
+        public bool LoadCalibrationFactors(string fileName, bool useFrequencyOffsetTable = false)
+        {
+            // Check that the file exists
+            if (File.Exists(fileName))
+            {
+                CalFactors = JsonSerializer.Deserialize<List<CalibrationFactor>>(File.ReadAllText(fileName));
+
+                // Set the device to RF Power mode and with the trigger off (free run)
+                SendCommand("M4T0");
+
+                // Select the appropriate table
+                if (useFrequencyOffsetTable)
+                    SendCommand("27.1SP"); // Enter Frequency Offset Mode
+                else
+                    SendCommand("27.0SP"); // Exit Frequency Offset Mode
+
+                // Clear the table
+                SendCommand("37.9SP");
+
+                // Store the calibration factors
+                foreach(CalibrationFactor factor in CalFactors)
+                {
+                    SendCommand("37.3SP" + String.Format("{0:F2}MZ{1:F2}CF", factor.Frequency * 1000, factor.CalFactor));
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool ZeroPowerSensor()
+        {
+            // Place the unit into RF Power mode and with the trigger off (free run)
+            SendCommand("M4T0");
+
+            // Zero the unit
+            SendCommand("ZR");
+            
+            // When a Zero command is sent we should wait for a data item to be ready to confirm that the Zero has been completed (SP22.3)
+            SendCommand("22.3SP");
+
+            // Wait for the data to be available
+            srqWait.Wait();
+
+            // Clear the SRQ Mask
+            SendCommand("22.0SP");
+
+            return true; //TODO: These return codes need to be updated with errors
+        }
+
+        public bool CalibratePowerSensor()
+        {
+            // Place the unit into RF Power mode and with the trigger off (free run)
+            SendCommand("M4T0");
+
+            // Turn the calibration source on
+            SendCommand("C1");
+
+            // Wait for calibration to complete
+            SendCommand("22.3SP");
+
+            // Wait for the data to be available
+            srqWait.Wait();
+
+            // Clear the SRQ Mask
+            SendCommand("22.0SP");
+
+            // Save the calibration
+            SendCommand("SC");
+
+            // Turn the calibration source off
+            SendCommand("C0");
+
+            return true; //TODO: These return codes need to be updated with errors
+        }
+
         private double ReadSciValue()
         {
             // The frequency is read in scientific notation as Hz
@@ -254,7 +349,8 @@ namespace HP8902A
             */
 
             // Read the Status Byte but discard for now
-            _ = gpibSession.ReadStatusByte();
+            //_ = gpibSession.ReadStatusByte();
+            var temp = gpibSession.ReadStatusByte();
 
             // Assume Data Ready and release the semaphore for now
             srqWait.Release();
--- HP8902A-Test/HP8902ATestHarness/Program.cs
+++ HP8902A-Test/HP8902ATestHarness/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.Json;
+using System.IO;
 using HP8902A;
 
 namespace HP8902ATestHarness
@@ -11,20 +13,88 @@ namespace HP8902ATestHarness
     {
         static void Main(string[] args)
         {
+            // HP 11792A Table
+            List<CalibrationFactor> calibrationFactors92A = new List<CalibrationFactor>();
+
+            calibrationFactors92A.Add(new CalibrationFactor(0.05, 100.0));
+            calibrationFactors92A.Add(new CalibrationFactor(2.0, 96.3));
+            calibrationFactors92A.Add(new CalibrationFactor(3.0, 94.8));
+            calibrationFactors92A.Add(new CalibrationFactor(4.0, 93.9));
+            calibrationFactors92A.Add(new CalibrationFactor(5.0, 92.9));
+            calibrationFactors92A.Add(new CalibrationFactor(6.0, 91.9));
+            calibrationFactors92A.Add(new CalibrationFactor(7.0, 91.1));
+            calibrationFactors92A.Add(new CalibrationFactor(8.0, 90.3));
+            calibrationFactors92A.Add(new CalibrationFactor(9.0, 89.3));
+            calibrationFactors92A.Add(new CalibrationFactor(10.0, 88.5));
+            calibrationFactors92A.Add(new CalibrationFactor(11.0, 87.5));
+            calibrationFactors92A.Add(new CalibrationFactor(12.4, 87.0));
+            calibrationFactors92A.Add(new CalibrationFactor(13.0, 86.1));
+            calibrationFactors92A.Add(new CalibrationFactor(14.0, 85.6));
+            calibrationFactors92A.Add(new CalibrationFactor(15.0, 85.4));
+            calibrationFactors92A.Add(new CalibrationFactor(16.0, 84.9));
+            calibrationFactors92A.Add(new CalibrationFactor(17.0, 84.6));
+            calibrationFactors92A.Add(new CalibrationFactor(18.0, 84.1));
+
+            string fileName = "CalFactors92A.json";
+            string jsonString = JsonSerializer.Serialize(calibrationFactors92A);
+            File.WriteAllText(fileName, jsonString);
+
+            // HP 11792A Table
+            List<CalibrationFactor> calibrationFactors22A = new List<CalibrationFactor>();
+
+            calibrationFactors22A.Add(new CalibrationFactor(0.05, 98.0));
+            calibrationFactors22A.Add(new CalibrationFactor(0.0001, 96.8));
+            calibrationFactors22A.Add(new CalibrationFactor(0.0003, 99.0));
+            calibrationFactors22A.Add(new CalibrationFactor(0.0005, 99.2));
+            calibrationFactors22A.Add(new CalibrationFactor(0.001, 99.2));
+            calibrationFactors22A.Add(new CalibrationFactor(0.003, 99.1));
+            calibrationFactors22A.Add(new CalibrationFactor(0.005, 99.1));
+            calibrationFactors22A.Add(new CalibrationFactor(0.01, 98.3));
+            calibrationFactors22A.Add(new CalibrationFactor(0.03, 98.0));
+            calibrationFactors22A.Add(new CalibrationFactor(0.1, 97.4));
+            calibrationFactors22A.Add(new CalibrationFactor(0.3, 95.8));
+            calibrationFactors22A.Add(new CalibrationFactor(0.5, 94.8));
+            calibrationFactors22A.Add(new CalibrationFactor(1.0, 92.8));
+            calibrationFactors22A.Add(new CalibrationFactor(1.5, 91.3));
+            calibrationFactors22A.Add(new CalibrationFactor(2.0, 89.9));
+            calibrationFactors22A.Add(new CalibrationFactor(2.6, 87.8));
+
+            fileName = "CalFactors22A.json";
+            jsonString = JsonSerializer.Serialize(calibrationFactors22A);
+            File.WriteAllText(fileName, jsonString);
+
             HP8902A.Device measuringReceiver = new Device(@"GPIB0::14::INSTR");
 
-            Console.WriteLine("Frequency value {0}Hz", measuringReceiver.MeasureFrequency());
+            // Frequency Tests
+            //Console.WriteLine("Frequency value {0}Hz", measuringReceiver.MeasureFrequency());
 
-            Console.WriteLine("Frequency error {0}Hz", measuringReceiver.MeasureFrequencyError(10000000));
+            //Console.WriteLine("Frequency error {0}Hz", measuringReceiver.MeasureFrequencyError(10000000));
 
-            Console.WriteLine("AM Modulation {0}%", measuringReceiver.MeasureAMModulationPercent());
+            //Console.WriteLine("AM Modulation {0}%", measuringReceiver.MeasureAMModulationPercent());
             
-            Console.WriteLine("FM Modulation Deviation {0}Hz", measuringReceiver.MeasureFMModulationDeviation());
+            //Console.WriteLine("FM Modulation Deviation {0}Hz", measuringReceiver.MeasureFMModulationDeviation());
 
-            Console.WriteLine("Phase Modulation Angle {0}Rad", measuringReceiver.MeasurePhaseModulationRadian());
-            Console.WriteLine("Phase Modulation Angle {0}Deg", measuringReceiver.MeasurePhaseModulationDegree());
+            //Console.WriteLine("Phase Modulation Angle {0}Rad", measuringReceiver.MeasurePhaseModulationRadian());
+            //Console.WriteLine("Phase Modulation Angle {0}Deg", measuringReceiver.MeasurePhaseModulationDegree());
 
-            Console.WriteLine("Modulation Frequency {0}Hz", measuringReceiver.MeasureModulationFrequency());
+            //Console.WriteLine("Modulation Frequency {0}Hz", measuringReceiver.MeasureModulationFrequency());
+
+            // RF Power Tests
+            Console.WriteLine("Loading Calibration Factors");
+
+            Console.WriteLine("Frequency Offset Table");
+            measuringReceiver.LoadCalibrationFactors("CalFactors92A.json", true);
+
+            Console.WriteLine("Base Table");
+            measuringReceiver.LoadCalibrationFactors("CalFactors22A.json", false);
+
+            // Zero the sensor
+            Console.WriteLine("Zeroing Sensor");
+            measuringReceiver.ZeroPowerSensor();
+
+            // Calibrate the power sensor
+            Console.WriteLine("Calibrating Sensor");
+            measuringReceiver.CalibratePowerSensor();
 
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
