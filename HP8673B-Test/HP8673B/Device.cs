2b8d2802312cdc6006936a37723de96dd8b85b1a
Tony Goodhew <Tony@schnauzergroup.com>
2022-05-20 13:28:32 -0700

    Moving test code from the 8902A project over here

--- HP8673B-Test/HP8673B/Device.cs
+++ HP8673B-Test/HP8673B/Device.cs
@@ -3,31 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
+using Ivi.Visa.FormattedIO;
 using NationalInstruments.Visa;
+using Ivi.Visa;
+using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace HP8673B
 {
     [Flags]
     public enum SRQMaskFlags : short
     {
-        /* Status Byte Bit Values
-         * Bit 7 - Change In Sweep Parameters
+        /* Bit 7 - Change in Sweep Parameters
          * Bit 6 - Asserted whenever there is an SRQ
          * Bit 5 - Entry Error
-         * Bit 4 - End Of Sweep
+         * Bit 4 - End of Sweep
          * Bit 3 - Source Settled
-         * Bit 2 - Change In Extended Status
+         * Bit 2 - Change in Extended Status Byte
          * Bit 1 - Front Panel Entry Complete
          * Bit 0 - Front Panel Key Pressed
         */
-        FrontPanelKey = 0x01,
-        FrontPanelComplete = 0x02,
-        ChangeExtendedStatus = 0x04,
+        FrontPanelKeyPressed = 0x01,
+        FrontPanelEntryComplete = 0x02,
+        ChangeInESB = 0x04,
         SourceSettled = 0x08,
         EndOfSweep = 0x10,
         EntryError = 0x20,
-        ChangedSweepParemeters = 0x80
+        SRQAssert = 0x40,
+        ChangedSweepParameters = 0x80
     }
 
     public class Device
@@ -37,6 +40,8 @@ namespace HP8673B
         private GpibSession gpibSession;
         private ResourceManager resManager;
         private SemaphoreSlim srqWait = new SemaphoreSlim(0, 1); // use a semaphore to wait for the SRQ events
+
+        private string lastCommand;
 
         public Device(string GPIBAddress)
         {
@@ -51,30 +56,84 @@ namespace HP8673B
 
             gpibSession.ServiceRequest += SRQHandler;
 
+            // Ensure the the SRQ mask is 0
+            SendCommand("RM0");
+
             // Send an instrument preset
             SendCommand("IP");
         }
 
+        public double SetCWFrequency(double frequency)
+        {
+            // Setup the SRQ to wait for source to be settled (RM)
+            string command = String.Format("RM{0:d}", SRQMaskFlags.SourceSettled);
+
+            SendCommand("RM8");
+
+            // Set the CW frequency in Hz (FR) 
+            SendCommand(String.Format("FR{0}HZ", frequency));
+
+            // Wait for the data to be available
+            srqWait.Wait();
+
+            // Clear the SRQ mask
+            SendCommand("RM0");
+
+            // Frequencies above 6.6GHz may not be set exactly due to multiplication of the baseband frequency
+            // so we should read back the frequency to provide that back for tasks such as LO use
+            // OK returns the current frequency that is locked
+            SendCommand("OK");
+
+            // Get the value
+            string result = "FR3000000000HZ";
+
+            result = gpibSession.FormattedIO.ReadString();
+
+            result = Regex.Match(result, @"\d+").Value;
+
+            return double.Parse(result);
+        }
+
+        public void SetPowerLevel(double power)
+        {
+            // Set the power level (LE)
+            SendCommand(String.Format("LE{0}DM", power));
+        }
+
         private void SendCommand(string command)
         {
+            lastCommand = command;
             gpibSession.FormattedIO.WriteLine(command);
         }
-        
+
+        private double ReadSciValue()
+        {
+            // The frequency is read in scientific notation as Hz
+            gpibSession.FormattedIO.Scanf<double>("%e", out double result);
+            return result;
+        }
+
         private void SRQHandler(object sender, Ivi.Visa.VisaEventArgs e)
         {
             /* Status Byte Bit Values
-             * Bit 7 - Change In Sweep Parameters
+             * Bit 7 - Change in Sweep Parameters
              * Bit 6 - Asserted whenever there is an SRQ
              * Bit 5 - Entry Error
-             * Bit 4 - End Of Sweep
+             * Bit 4 - End of Sweep
              * Bit 3 - Source Settled
-             * Bit 2 - Change In Extended Status
+             * Bit 2 - Change in Extended Status Byte
              * Bit 1 - Front Panel Entry Complete
              * Bit 0 - Front Panel Key Pressed
-            */
+             */
 
             // Read the Status Byte but discard for now
-            _ = gpibSession.ReadStatusByte();
+            // This has to be done via polling the Status Byte till zero
+            // TODO: Clean up this debug code
+            while (true)
+            {
+                var statusbyte = gpibSession.ReadStatusByte();
+                if (statusbyte == 0) break;
+            }
 
             // Assume Data Ready and release the semaphore for now
             srqWait.Release();
