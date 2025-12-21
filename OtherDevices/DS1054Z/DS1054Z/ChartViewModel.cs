727f0014f364beaf7774e295ea2152b5a0c8cab8
Tony Goodhew <Tony@schnauzergroup.com>
2025-12-20 20:05:19 -0800

    Fixed OutOfRange exceptions and the channel text

---
+++ OtherDevices/DS1054Z/DS1054Z/ChartViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS1054Z
+{
+    public class DataPoint
+    {
+        public int X { get; set; }
+        public byte Y { get; set; }
+    }
+
+    public class ChartViewModel
+    {
+        public List<DataPoint> ByteSeries { get; }
+
+        public ChartViewModel(byte[] data)
+        {
+            ByteSeries = ConvertBytes(data);
+        }
+
+        public List<DataPoint> ConvertBytes(byte[] bytes)
+        {
+            var list = new List<DataPoint>();
+
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                list.Add(new DataPoint { X = i, Y = bytes[i] });
+            }
+
+            return list;
+        }
+    }
+}
